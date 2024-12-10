eadfbb77014e145be2ceb30ce55c2cede86a2443
maiorano16 <mfmaiorano16@gmail.com>
2024-12-10 13:53:30 +0100

    add new circularBar widget

---
+++ banking_app_1/lib/widgets/circular_progres_bar_balance.dart
@@ -0,0 +1,36 @@
+import 'package:multi_circular_slider/multi_circular_slider.dart';
+import 'package:flutter/material.dart';
+ 
+class CircularBar extends StatelessWidget {
+  final List<double> values;
+  final List<Color> colors; 
+  final String label; 
+  final String innerText;
+
+  const CircularBar({
+    Key? key,
+    required this.values,
+    required this.colors,
+    required this.label,
+    required this.innerText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiCircularSlider(
+      size: MediaQuery.of(context).size.width * 0.8,
+      progressBarType: MultiCircularSliderType.circular,
+      values: values,
+      colors: colors,
+      showTotalPercentage: true,
+      label: label,
+      animationDuration: const Duration(milliseconds: 500),
+      animationCurve: Curves.easeIn,
+      innerIcon: Icon(Icons.account_balance_wallet),
+      innerWidget: Text(innerText),
+      trackColor: Colors.white,
+      progressBarWidth: 52.0,
+      trackWidth: 52.0,
+    );
+  }
+}
