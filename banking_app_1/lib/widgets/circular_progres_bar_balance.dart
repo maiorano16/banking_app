3e276bbc612eed11cafd66e5b501933839aa9f0d
maiorano16 <mfmaiorano16@gmail.com>
2024-12-12 10:33:31 +0100

    Update circularBar for balance

--- banking_app_1/lib/widgets/circular_progres_bar_balance.dart
+++ banking_app_1/lib/widgets/circular_progres_bar_balance.dart
@@ -1,45 +1,28 @@
-import 'package:circular_gradient_progress/circular_gradient_progress.dart';
 import 'package:flutter/material.dart';
+import 'package:circular_gradient_progress/circular_gradient_progress.dart';
 
-class CircularProgressPage extends StatefulWidget {
-  @override
-  _CircularProgressPageState createState() => _CircularProgressPageState();
-}
+class CircularGradientProgress extends StatelessWidget {
+  final List<double> sweepAngles;
+  final List<List<Color>> gradientColors;
+  final List<Color> backgroundColors;
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
-  final duration = Duration(seconds: 2);
-
- 
-  final List<Color> backgroundColors = [
-    Color(0xFF2196F3).withOpacity(0.2), 
-    Color(0xFF4CAF50).withOpacity(0.2), 
-    Color(0xFFF44336).withOpacity(0.2), 
-  ];
-
- 
-  final List<List<Color>> gradientColors = [
-    [Color(0xFF2196F3), Color(0xFF1976D2)], 
-    [Color(0xFF4CAF50), Color(0xFF388E3C)], 
-    [Color(0xFFF44336), Color(0xFFD32F2F)],
-  ];
+  const CircularGradientProgress({
+    Key? key,
+    required this.sweepAngles,
+    required this.gradientColors,
+    required this.backgroundColors,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Circular Gradient Progress'),
-        backgroundColor: Colors.white,
-      ),
-      body: Center(
-        child: CircularGradientCombineWidget(
-          size: 120, 
-          duration: duration, 
-          centerCircleSizeRatio: 0.05, 
-          sweepAngles: const [200, 720, 860], 
-          backgroundColors: backgroundColors, 
-          gradientColors: gradientColors, 
-        ),
-      ),
+    return CircularGradientCombineWidget(
+      size: 220,
+      duration: const Duration(seconds: 3),
+      centerCircleSizeRatio: 0.11,
+      sweepAngles: sweepAngles,
+      gapRatio: 0.15,
+      backgroundColors: backgroundColors,
+      gradientColors: gradientColors,
     );
   }
 }
