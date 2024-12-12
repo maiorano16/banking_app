0c8f9215bd8ff5728c99895f2fd0b3b6030de50c
maiorano16 <mfmaiorano16@gmail.com>
2024-12-12 10:52:48 +0100

    Create flippableCard.dart

---
+++ banking_app_1/lib/widgets/flippableCard.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class FlippableCard extends StatefulWidget {
+  final Widget front;
+  final Widget back;
+
+  const FlippableCard({
+    Key? key,
+    required this.front,
+    required this.back,
+  }) : super(key: key);
+
+  @override
+  _FlippableCardState createState() => _FlippableCardState();
+}
+
+class _FlippableCardState extends State<FlippableCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool isFront = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _flipCard() {
+    if (isFront) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+    setState(() {
+      isFront = !isFront;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _flipCard,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          final angle = _animation.value * 3.14159;
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(angle),
+            child: isFront
+                ? widget.front
+                : Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.identity()..rotateY(3.14159),
+                    child: widget.back,
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
