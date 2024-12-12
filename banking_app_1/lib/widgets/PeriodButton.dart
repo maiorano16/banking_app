76bf0a59ac16713ecebeb1e55084a3e7583be41c
maiorano16 <mfmaiorano16@gmail.com>
2024-12-12 10:53:01 +0100

    Update periodButton.dart

--- banking_app_1/lib/widgets/PeriodButton.dart
+++ banking_app_1/lib/widgets/PeriodButton.dart
@@ -4,13 +4,13 @@ class PeriodButton extends StatelessWidget {
   final String label;
   final String period;
   final VoidCallback onPressed;
-  final bool isSelected; // Aggiunto parametro
+  final bool isSelected; 
 
   const PeriodButton({
     required this.label,
     required this.period,
     required this.onPressed,
-    required this.isSelected, // Richiesto
+    required this.isSelected, 
     Key? key,
   }) : super(key: key);
 
@@ -20,19 +20,19 @@ class PeriodButton extends StatelessWidget {
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: isSelected
-            ? Colors.blueAccent // Colore per il bottone selezionato
-            : Colors.grey,       // Colore per i bottoni non selezionati
+            ? Colors.blueAccent 
+            : Colors.grey,      
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
           side: BorderSide(
-            color: isSelected ? Colors.blue : Colors.transparent, // Bordo
+            color: isSelected ? Colors.blue : Colors.transparent, 
           ),
         ),
       ),
       child: Text(
         label,
         style: TextStyle(
-          color: isSelected ? Colors.white : Colors.black, // Testo visibile
+          color: isSelected ? Colors.white : Colors.black, 
         ),
       ),
     );
