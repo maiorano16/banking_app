c2c490736deb07add354b9303f715562b28d96c6
maiorano16 <mfmaiorano16@gmail.com>
2024-12-16 14:05:12 +0100

    Fix card Page

--- banking_app_1/lib/pages/cardScreen.dart
+++ banking_app_1/lib/pages/cardScreen.dart
@@ -1,3 +1,4 @@
+import 'package:banking_app_1/pages/editCardScreen.dart';
 import 'package:banking_app_1/widgets/carousel_cards.dart';
 import 'package:banking_app_1/models/card_model.dart';
 import 'package:banking_app_1/models/transaction_model.dart';
@@ -10,22 +11,29 @@ class CardScreen extends StatefulWidget {
 }
 
 class _CardScreenState extends State<CardScreen> {
-  List<int> cardOrder = [];
+  List<Carta> carte = []; // Cambia da List<int> a List<Carta>
   List<Transaction> transactions = [];
-   String selectedCardId = '';
+  String selectedCardId = '';
 
-   @override
+  @override
   void initState() {
     super.initState();
     loadTransactionFromJson().then((loadedTransactions) {
       setState(() {
-        transactions = loadedTransactions; 
+        transactions = loadedTransactions;
       });
     }).catchError((e) {
       print("Errore durante il caricamento delle transazioni: $e");
     });
+    // Carica le carte iniziali
+    loadCarteFromJson().then((loadedCards) {
+      setState(() {
+        carte = loadedCards;
+      });
+    }).catchError((e) {
+      print("Errore durante il caricamento delle carte: $e");
+    });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,54 +43,50 @@ class _CardScreenState extends State<CardScreen> {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {
-              print('IconButton cliccato');
+            onPressed: () async {
+              // Naviga alla schermata di aggiunta carta e aspettati una nuova carta
+              final newCard = await Navigator.push<Carta>(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddCardScreen(),
+                ),
+              );
+
+              if (newCard != null) {
+                setState(() {
+                  // Aggiungi la nuova carta alla lista delle carte
+                  carte.add(newCard);
+                  selectedCardId = newCard.cardId; // Se vuoi selezionare la carta appena aggiunta
+                });
+              }
             },
           ),
         ],
       ),
-      body: FutureBuilder<List<Carta>>(
-        future: loadCarteFromJson(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Errore: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('Nessuna carta disponibile.'));
-          } else {
-            List<Carta> carte = snapshot.data!;
-            if (cardOrder.isEmpty) {
-               cardOrder = List.generate(carte.length, (index) => index);
-               selectedCardId = carte[0].cardId;
-            }
-            return Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  child: Carousel(
-                    title: 'Carosello',
-                    carte: carte,
-                    selectedCardId: selectedCardId,
-                    onCardSelected: (cardId) {
-                      setState(() {
-                        selectedCardId = cardId;
-                      });
-                    },
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            child: Carousel(
+              title: 'Carosello',
+              carte: carte,
+              selectedCardId: selectedCardId,
+              onCardSelected: (cardId) {
+                setState(() {
+                  selectedCardId = cardId;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 40),
+          selectedCardId.isNotEmpty
+              ? Expanded(
+                  child: TransactionsListPage(
+                    transactions: filterTransactionsByCardId(selectedCardId),
                   ),
-                ),
-                const SizedBox(height: 40),
-                selectedCardId.isNotEmpty
-                    ? Expanded(
-                        child: TransactionsListPage(
-                          transactions: filterTransactionsByCardId(selectedCardId),
-                        ),
-                      )
-                    : const Center(child: Text('Seleziona una carta per vedere le transazioni.')),
-              ],
-            );
-          }
-        },
+                )
+              : const Center(child: Text('Seleziona una carta per vedere le transazioni.')),
+        ],
       ),
     );
   }
