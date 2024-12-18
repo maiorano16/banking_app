3705b2dc5ce005a87ec7ac7587941030f581df95
maiorano16 <mfmaiorano16@gmail.com>
2024-12-18 17:38:37 +0100

    Fix addCardScreen.dart

--- banking_app_1/lib/pages/addCardScreen.dart
+++ banking_app_1/lib/pages/addCardScreen.dart
@@ -1,5 +1,8 @@
+import 'package:banking_app_1/utility/validations_utils.dart';
+import 'package:banking_app_1/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:banking_app_1/models/card_model.dart';
+
 
 class AddCardScreen extends StatefulWidget {
   @override
@@ -31,15 +34,6 @@ class _AddCardScreenState extends State<AddCardScreen> {
     circuito = '';
   }
 
-  bool isValidExpirationDate(String date) {
-    RegExp exp = RegExp(r"^(0[1-9]|1[0-2])\/([0-9]{2})$");
-    return exp.hasMatch(date);
-  }
-
-  bool isValidCardNumber(String number) {
-    return number.length == 16 && RegExp(r'^\d+$').hasMatch(number);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,8 +46,8 @@ class _AddCardScreenState extends State<AddCardScreen> {
           key: _formKey,
           child: ListView(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'ID Carta'),
+              InputField(
+                labelText: 'ID Carta',
                 onChanged: (value) => cardId = value,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -62,24 +56,21 @@ class _AddCardScreenState extends State<AddCardScreen> {
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration:
-                    const InputDecoration(labelText: 'Scadenza Carta (MM/YY)'),
+              InputField(
+                labelText: 'Scadenza Carta (MM/YY)',
                 onChanged: (value) => scadenzaCarta = value,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'La data di scadenza è obbligatoria.';
                   }
-                  if (!isValidExpirationDate(value)) {
+                  if (!ValidationUtils.isValidExpirationDate(value)) {
                     return 'Inserisci una data di scadenza valida (MM/YY).';
                   }
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Saldo Carta'),
+              InputField(
+                labelText: 'Saldo Carta',
                 onChanged: (value) {
                   saldoCarta = double.tryParse(value) ?? 0.0;
                 },
@@ -92,71 +83,52 @@ class _AddCardScreenState extends State<AddCardScreen> {
                   }
                   return null;
                 },
+                keyboardType: TextInputType.number,
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Numero Carta'),
+              InputField(
+                labelText: 'Numero Carta',
                 onChanged: (value) => numeroCarta = value,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Il numero della carta è obbligatorio.';
                   }
-                  if (!isValidCardNumber(value)) {
+                  if (!ValidationUtils.isValidCardNumber(value)) {
                     return 'Il numero della carta deve essere composto da 16 cifre.';
                   }
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Tipo Account'),
+              InputField(
+                labelText: 'Tipo Account',
                 onChanged: (value) => tipoAccount = value,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Il tipo di account è obbligatorio.';
-                  }
-                  return null;
-                },
+                validator: (value) => value == null || value.isEmpty
+                    ? 'Il tipo di account è obbligatorio.'
+                    : null,
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'IBAN'),
+              InputField(
+                labelText: 'IBAN',
                 onChanged: (value) => iban = value,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'L\'IBAN è obbligatorio.';
-                  }
-                  return null;
-                },
+                validator: (value) =>
+                    value == null || value.isEmpty ? 'L\'IBAN è obbligatorio.' : null,
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Tipo di Carta'),
+              InputField(
+                labelText: 'Tipo di Carta',
                 onChanged: (value) => tipoCarta = value,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Il tipo di carta è obbligatorio.';
-                  }
-                  return null;
-                },
+                validator: (value) => value == null || value.isEmpty
+                    ? 'Il tipo di carta è obbligatorio.'
+                    : null,
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Circuito'),
+              InputField(
+                labelText: 'Circuito',
                 onChanged: (value) => circuito = value,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Il circuito è obbligatorio.';
-                  }
-                  return null;
-                },
+                validator: (value) =>
+                    value == null || value.isEmpty ? 'Il circuito è obbligatorio.' : null,
               ),
               const SizedBox(height: 32),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-
                       Carta newCard = Carta(
                         cardId: cardId,
                         scadenzaCarta: scadenzaCarta,
@@ -167,9 +139,7 @@ class _AddCardScreenState extends State<AddCardScreen> {
                         tipoCarta: tipoCarta,
                         circuito: circuito,
                       );
-
-                      Navigator.pop(
-                          context, newCard); 
+                      Navigator.pop(context, newCard);
                     }
                   },
                   child: const Text('Salva Carta'),
