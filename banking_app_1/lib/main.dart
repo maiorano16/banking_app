ffd31f6a4d4185869d23f24c44bd4a5fa67d4e03
maiorano16 <mfmaiorano16@gmail.com>
2024-12-20 10:16:54 +0100

    Delede unused import in main

--- banking_app_1/lib/main.dart
+++ banking_app_1/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:banking_app_1/pages/balanceScreen.dart';
-import 'package:banking_app_1/pages/cardScreen.dart';
 import 'package:banking_app_1/pages/mainAccountScreen.dart';
-import 'package:banking_app_1/pages/userProfileScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
