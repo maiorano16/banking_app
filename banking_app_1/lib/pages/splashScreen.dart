9cc206f90ac1532ddfc9f58bbc72df41ab17f663
maiorano16 <mfmaiorano16@gmail.com>
2025-01-07 15:15:46 +0100

    fix spalshscreen

--- banking_app_1/lib/pages/splashScreen.dart
+++ banking_app_1/lib/pages/splashScreen.dart
@@ -1,5 +1,6 @@
-import 'package:banking_app_1/pages/chatBot.dart';
-import 'package:banking_app_1/pages/settingScreen.dart';
+import 'package:banking_app_1/pages/balanceScreen.dart';
+import 'package:banking_app_1/pages/cardScreen.dart';
+import 'package:banking_app_1/pages/mainAccountScreen.dart';
 import 'package:banking_app_1/pages/userProfileScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -17,22 +18,8 @@ class _SplashScreenState extends State<SplashScreen> {
       // Transizione con Fade e Scale
       Navigator.pushReplacement(
         context,
-        PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => ChatBotScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            // FadeIn e Scale
-            var fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(animation);
-            var scaleAnimation = Tween(begin: 0.8, end: 1.0).animate(animation);
-            return ScaleTransition(
-              scale: scaleAnimation,
-              child: FadeTransition(
-                opacity: fadeAnimation,
-                child: child,
-              ),
-            );
-          },
-        ),
-      );
+        MaterialPageRoute(builder: (context) => MainAccountPage()),
+      );  // Fixed the missing comma here
     });
   }
 
